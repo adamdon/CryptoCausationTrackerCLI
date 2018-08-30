4ea42a91c96361465f8513fd092ad8a1459af572
Adam Don <contact@adamdon.co.uk>
2018-08-30 18:52:44 +0100

    Test data outputing

---
+++ CryptoCausationTrackerCLI/Message.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoCausationTrackerCLI
+{
+
+    public class Message
+    {
+        public string exchange { get; set; }
+        public string cryptocurrency { get; set; }
+        public string basecurrency { get; set; }
+        public string type { get; set; }
+        public string price { get; set; }
+        public object size { get; set; }
+        public string bid { get; set; }
+        public string ask { get; set; }
+        public object open { get; set; }
+        public string high { get; set; }
+        public string low { get; set; }
+        public string volume { get; set; }
+        public string timestamp { get; set; }
+    }
+
+}
--- CryptoCausationTrackerCLI/Program.cs
+++ CryptoCausationTrackerCLI/Program.cs
@@ -3,13 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using Newtonsoft.Json.Linq;
+using Satori.Rtm;
+using Satori.Rtm.Client;
+using System.IO;
+using System.Runtime.Serialization.Json;
 
 namespace CryptoCausationTrackerCLI
 {
     class Program
     {
-        static void Main(string[] args)
+        const string endpoint = "wss://open-data.api.satori.com";
+        const string appkey = "08887F4cC5Fb788f899F3bc2bF2deA75";
+        const string channel = "cryptocurrency-market-data";
+
+        static string strOutputText = "Error1";
+        string strType;
+
+        static void Main()
         {
+            // Log messages from SDK to the console
+            Trace.Listeners.Add(new ConsoleTraceListener());
+
+            IRtmClient client = new RtmClientBuilder(endpoint, appkey).Build();
+            client.OnEnterConnected += cn => Console.WriteLine("Connected to Satori RTM!");
+            client.Start();
+
+            // Create subscription observer to observe channel subscription events 
+            var observer = new SubscriptionObserver();
+
+            observer.OnSubscriptionData += (ISubscription sub, RtmSubscriptionData data) =>
+            {
+                foreach (JToken msg in data.Messages)
+                {
+                    //Console.WriteLine("Got message: " + msg);
+                    strOutputText = msg.ToString();
+                    Console.WriteLine("Coin: " + ReadToObject(strOutputText).cryptocurrency);
+                    Console.WriteLine("Price: " + ReadToObject(strOutputText).price);
+                    //strType
+                    //new Message = ReadToObject(strOutputText);
+                }
+            };
+
+
+            client.CreateSubscription(channel, SubscriptionModes.Simple, observer);
+
+            Console.ReadKey();
+
+            // Stop and clean up the client before exiting the program
+            client.Dispose().Wait();
         }
+
+        public static Message ReadToObject(string json)
+        {
+            Message deserializedMessage = new Message();
+            MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(json));
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(deserializedMessage.GetType());
+            deserializedMessage = ser.ReadObject(ms) as Message;
+            ms.Close();
+            return deserializedMessage;
+        }
+
     }
 }
